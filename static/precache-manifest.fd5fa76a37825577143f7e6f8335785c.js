12f54160754a93a729865872ce6737a210ff42a4
Elsammit <hhashimo25@gmail.com>
2020-08-02 18:18:18 +0900

    modify Mobile

---
+++ static/precache-manifest.fd5fa76a37825577143f7e6f8335785c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1239bb8c9315ead93518198cfed9d9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "35817d0f18f0deed9c0f",
+    "url": "./static/css/main.286cdb41.chunk.css"
+  },
+  {
+    "revision": "ce3aad81c08c58e1ff65",
+    "url": "./static/js/2.35b2eed8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.35b2eed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35817d0f18f0deed9c0f",
+    "url": "./static/js/main.d14bd0f4.chunk.js"
+  },
+  {
+    "revision": "039a0d0ec1f31d95aee2",
+    "url": "./static/js/runtime-main.16d4e7cc.js"
+  },
+  {
+    "revision": "4b88db3d25d1bad5d5b1396917c714f8",
+    "url": "./static/media/hit_mogura.4b88db3d.png"
+  },
+  {
+    "revision": "b295d30670f6ab0737c6fb8dcaf9e557",
+    "url": "./static/media/mogura2.b295d306.png"
+  },
+  {
+    "revision": "46ee5b4aa667bdfc861879afe230c65e",
+    "url": "./static/media/shibafu.46ee5b4a.png"
+  }
+]);
